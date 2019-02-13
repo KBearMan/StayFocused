d4fa763b82d00b54ce11bb1ce3eec1375ff2d2b4
kbearman <10565345+KBearMan@users.noreply.github.com>
2019-02-12 19:26:57 -0500

    initial commit

---
+++ app/src/main/java/com/bearhat/stayfocused/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bearhat.stayfocused
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
